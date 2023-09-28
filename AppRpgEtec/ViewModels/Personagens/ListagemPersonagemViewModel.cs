c5aac066da52648a61e3fbde67f6991ca022fcf9
erikasntn <erikaln54@gmail.com>
2023-09-27 22:12:11 -0300

    Utilizando API do google

---
+++ AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,45 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+using AppRpgEtec.ViewModels.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+
+    {
+        private PersonagemService pService;
+
+        public ObservableCollection<Personagem> Personagens { get; set; }
+
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+
+            _ = ObterPersonagem();
+        }
+        //prox elementos da classe aq
+
+        public async Task ObterPersonagem()
+        {
+            try
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens));
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    } //fim da classe 
+}
